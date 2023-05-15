6cdc3bee0d3ce5b9b5dcaae1079210a15ac6d54e
radheyjhorar <shyamjhorar700@gmail.com>
2023-05-15 19:06:49 +0530

    something new

---
+++ concat-method.js
@@ -0,0 +1,12 @@
+let arr1 = ["radhey ", "sumit ", "rahul ", "jonny"]
+let arr2 = ["ehamad ", "jamson ", "keshav "]
+let arr3 = ["ramesh ", "kartik ", "bhumi ", "sunil"]
+let cnct = arr1.concat(arr2, arr3)
+console.log(cnct)
+let adString = arr1.concat("rudhra")
+console.log(adString)
+
+let arr = [1,2,[1,2],3,4,[3,4],5,6,[5,6],7,8,[7,8]]
+let flt = arr.flat();
+console.log(arr)
+console.log(flt)
